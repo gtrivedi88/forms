0d14a8f66a64762d612ea14198da5fa8b6b04c21
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2023-12-27 05:06:54 +0530

    Update db.py

--- taxonomy_manager/db.py
+++ taxonomy_manager/db.py
@@ -1,39 +1,82 @@
-from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired
-from wtforms import StringField, SubmitField, TextAreaField, DateField, BooleanField, SelectField
-from wtforms import DateField
+from db import db
+import uuid
+from datetime import datetime
 
-# Below are the form classes for each taxonomy. Each is based on the wtforms
-# specification. However, there are some additional variables to help with a
-# few things:
-#
-#   form_choices        Used with forms that contain SelectField and
-#                       SelectMultipleField. In some cases, the choices option
-#                       for these two field types are usually taken from another
-#                       table. which means they need to be set within the
-#                       context of the Flask app. So this variable sets a
-#                       mapping to data from another table, which the
-#                       utils.get_choices_for_selectfields method renders and
-#                       adds to the choices option of the relevant field. The
-#                       mapping is as follows:
-#                           'model'     The name of the model to pull data from.
-#                           'value'     The column to use for the select field value
-#                           'label'     The column to use for the select field label
-#   mapped_data         This variable acts as a trigger to let the Flask app
-#                       know that secondary table mapping is used. This is used
-#                       to both get data and set data to the secondary table.
+# Below are the models for each taxonomy
+# They use the standard SQLAlchemy modeling, but with a few added bits a pieces:
+#   __tablename__       SQLAlchemy attribute to map the model to a specific table
+#   __table_args__      SQLAlchemy attribute to set options for the model.
+#                       Mostly used to define the PGSQL schema.
+#   __uuid__            Defines the field to use as the UUID. Since the UUID
+#                       field name is different for each model, this is a way to
+#                       Standardize the field names
+#   __term__            Defines the field to use as the term name. This is
+#                       similar in function as the UUID.
+
+# OPL DATA MODELS
+
+# class OplPartner(db.Model):
+#     __tablename__ = 'partners'
+#     __table_args__ = {'schema': 'brand_opl'}
+#     __uuid__ = "partner_id"
+#     __term__ = "partner_name"
+#     __hidden__ = True
+#     partner_id = db.Column(db.String, primary_key=True)
+#     partner_name = db.Column(db.String)
+#     persona_id = db.Column(db.String)
+#     ccs_data_metadata_partner_attributes = db.relationship('PartnerAttribute', back_populates='brand_opl_partner')
+
+class Product(db.Model):
+    __tablename__ = 'product'
+    __table_args__ = {'schema': 'brand_opl'}
+    __uuid__ = "product_id"
+    __term__ = "product_name"
+    __hidden__ = True
+    product_id = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
+    product_name = db.Column(db.String(255), nullable=False)
+    product_description = db.Column(db.String)
+    upcoming_change = db.Column(db.Boolean)
+    deprecated = db.Column(db.Boolean)
+    product_status = db.Column(db.String(255))
+    last_updated = db.Column(db.Date)
+    created = db.Column(db.Date)
+    product_status_detail = db.Column(db.String(255))
+    # ccs_data_metadata_product_attributes = db.relationship('ProductAttribute', lazy='subquery', back_populates='brand_opl_product')
+    # brand_opl_product_alias = db.relationship('OplProductAlias', lazy='subquery', back_populates='brand_opl_product')
+
+    # Add a relationship to the ProductType model
+    product_type_maps = db.relationship('ProductTypeMap', backref='product', lazy='subquery')
+
+class ProductType(db.Model):
+    __tablename__ = 'product_types'
+    __table_args__ = {'schema': 'brand_opl'}
+
+    type_id = db.Column(db.String, primary_key=True)
+    product_type = db.Column(db.String(255), nullable=False)
+
+class ProductTypeMap(db.Model):
+    __tablename__ = 'product_types_map'
+    __table_args__ = {'schema': 'brand_opl'}
+
+    product_id = db.Column(db.String, db.ForeignKey('brand_opl.product.product_id'), primary_key=True)
+    type_id = db.Column(db.String, db.ForeignKey('brand_opl.product_types.type_id'), primary_key=True)
 
 
-class MyForm(FlaskForm):
-    product_name = StringField('Product Name *', validators=[DataRequired()])
-    product_description = TextAreaField('Product Description')
-    upcoming_change = BooleanField('Upcoming Change')
-    deprecated = BooleanField('Deprecated')
-    product_status = StringField('Product Status')
-    product_status_detail = TextAreaField('Product Status Detail')
-    product_type = SelectField('Product Type', validators=[DataRequired()])
-    product_portfolio = SelectField('Portfolio')
-    submit = SubmitField('Add Product')
+class ProductPortfolios(db.Model):
+    __tablename__ = 'product_portfolios'
+    __table_args__ = {'schema': 'brand_opl'}
+
+    category_id = db.Column(db.String, primary_key=True)
+    category_name = db.Column(db.String(255), nullable=False)
+
+    # Add a relationship to the ProductPortfolioMap model
+    product_portfolio_map = db.relationship('ProductPortfolioMap', backref='portfolio', lazy='dynamic')
 
 
 
+class ProductPortfolioMap(db.Model):
+    __tablename__ = 'product_portfolio_map'
+    __table_args__ = {'schema': 'brand_opl'}
+
+    product_id = db.Column(db.String, db.ForeignKey('brand_opl.product.product_id'), primary_key=True)
+    category_id = db.Column(db.String, db.ForeignKey('brand_opl.product_portfolios.category_id'), primary_key=True)
