cc8d9bde0a9e9b6aeb0a1455f0256fbf18e0b326
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2023-12-27 02:49:42 +0530

    Update db.py

--- taxonomy_manager/db.py
+++ taxonomy_manager/db.py
@@ -1,1 +1,1 @@
-RuntimeError: The current Flask app is not registered with this 'SQLAlchemy' instance. Did you forget to call 'init_app', or did you create multiple 'SQLAlchemy' instances?
+ValueError: invalid literal for int() with base 10: '9e9235c9-b30c-4ed3-88ac-ce18ae33054a'
