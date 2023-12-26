d943e3bc2257645c7550ef16038359292566f547
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2023-12-27 05:04:41 +0530

    Update db.py

--- taxonomy_manager/db.py
+++ taxonomy_manager/db.py
@@ -1,88 +1,39 @@
-from flask import Flask, render_template, flash
-from flask_sqlalchemy import SQLAlchemy
-from forms import MyForm
-from models import db, Product, ProductType, ProductTypeMap, ProductPortfolios, ProductPortfolioMap
-from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, TextAreaField, DateField, BooleanField, SelectField
+from wtforms import DateField
 
-# For troubleshooting
-import logging
-logging.basicConfig()
-logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+# Below are the form classes for each taxonomy. Each is based on the wtforms
+# specification. However, there are some additional variables to help with a
+# few things:
+#
+#   form_choices        Used with forms that contain SelectField and
+#                       SelectMultipleField. In some cases, the choices option
+#                       for these two field types are usually taken from another
+#                       table. which means they need to be set within the
+#                       context of the Flask app. So this variable sets a
+#                       mapping to data from another table, which the
+#                       utils.get_choices_for_selectfields method renders and
+#                       adds to the choices option of the relevant field. The
+#                       mapping is as follows:
+#                           'model'     The name of the model to pull data from.
+#                           'value'     The column to use for the select field value
+#                           'label'     The column to use for the select field label
+#   mapped_data         This variable acts as a trigger to let the Flask app
+#                       know that secondary table mapping is used. This is used
+#                       to both get data and set data to the secondary table.
 
-app = Flask(__name__)
-<data>
 
-db.init_app(app)
+class MyForm(FlaskForm):
+    product_name = StringField('Product Name *', validators=[DataRequired()])
+    product_description = TextAreaField('Product Description')
+    upcoming_change = BooleanField('Upcoming Change')
+    deprecated = BooleanField('Deprecated')
+    product_status = StringField('Product Status')
+    product_status_detail = TextAreaField('Product Status Detail')
+    product_type = SelectField('Product Type', validators=[DataRequired()])
+    product_portfolio = SelectField('Portfolio')
+    submit = SubmitField('Add Product')
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
 
-@app.route('/opl/add-product', methods=['GET', 'POST'])
-def add_product():
-    form = MyForm()
-    success_message = None
-    show_form = True
 
-    # Initialize the variables here
-    formatted_created_date = None
-    formatted_last_updated_date = None
-
-    # Populate product type choices
-    form.product_type.choices = [(ptype.type_id, ptype.product_type) for ptype in ProductType.query.all()]
-
-    # Populate product portfolio choices
-    form.product_portfolio.choices = [(portfolio.category_id, portfolio.category_name) for portfolio in ProductPortfolios.query.all()]
-
-    if form.validate_on_submit():
-        created_date = datetime.now()
-
-        new_product = Product(
-            product_name=form.product_name.data,
-            product_description=form.product_description.data,
-            upcoming_change=form.upcoming_change.data,
-            deprecated=form.deprecated.data,
-            product_status=form.product_status.data,
-            last_updated=created_date,
-            created=created_date,
-            product_status_detail=form.product_status_detail.data
-        )
-        db.session.add(new_product)
-        db.session.commit()
-
-        selected_product_type_id = form.product_type.data
-
-        product_type_map = ProductTypeMap(product_id=new_product.product_id, type_id=selected_product_type_id)
-        db.session.add(product_type_map)
-        db.session.commit()
-
-        selected_portfolio_id = form.product_portfolio.data
-
-        product_portfolio_map = ProductPortfolioMap(product_id=new_product.product_id, category_id=selected_portfolio_id)
-        db.session.add(product_portfolio_map)
-        db.session.commit()
-
-        formatted_created_date = created_date.strftime('%Y-%m-%d')
-        formatted_last_updated_date = created_date.strftime('%Y-%m-%d')
-
-        success_message = f'Successfully added the product: {form.product_name.data}'
-        show_form = False
-
-    return render_template('opl/add.html', form=form, success_message=success_message,
-                           show_form=show_form, formatted_created_date=formatted_created_date,
-                           formatted_last_updated_date=formatted_last_updated_date)
-
-@app.route('/opl/edit-product', methods=['GET', 'POST'])
-def edit_product():
-    form = MyForm()
-
-    if form.validate_on_submit():
-        # Logic for editing the product
-        return render_template('opl/edit.html', form=form, success_message=f'Successfully edited: {form.product_id.data}, {form.product_name.data}')
-
-    return render_template('opl/edit.html', form=form)
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
