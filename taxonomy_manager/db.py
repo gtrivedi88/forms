9fca1d2e86117b07b501bf5703251b004a38dc38
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2023-12-27 00:10:16 +0530

    Update db.py

--- taxonomy_manager/db.py
+++ taxonomy_manager/db.py
@@ -1,20 +1,9 @@
-INFO:sqlalchemy.engine.Engine:BEGIN (implicit)
-INFO:sqlalchemy.engine.Engine:INSERT INTO brand_opl.product (id, product_name, product_description, upcoming_change, deprecated, product_status, last_updated, created, product_status_detail) VALUES (%(id)s, %(product_name)s, %(product_description)s, %(upcoming_change)s, %(deprecated)s, %(product_status)s, %(last_updated)s, %(created)s, %(product_status_detail)s)
-INFO:sqlalchemy.engine.Engine:[generated in 0.00026s] {'id': 'cc5a9164-48e1-4325-b5dc-54ca784f5a62', 'product_name': 'dfdf', 'product_description': '', 'upcoming_change': False, 'deprecated': False, 'product_status': '', 'last_updated': datetime.date(2023, 12, 28), 'created': datetime.date(2023, 12, 28), 'product_status_detail': ''}
+/home/gtrivedi/git/gitlab/opl-ui/app.py:41: SAWarning: Column 'brand_opl.product.product_id' is marked as a member of the primary key for table 'brand_opl.product', but has no Python-side or server-side default generator indicated, nor does it indicate 'autoincrement=True' or 'nullable=True', and no explicit value is passed.  Primary key columns typically may not store NULL.
+  db.session.commit()
+INFO:sqlalchemy.engine.Engine:INSERT INTO brand_opl.product (product_name, product_description, upcoming_change, deprecated, product_status, last_updated, created, product_status_detail) VALUES (%(product_name)s, %(product_description)s, %(upcoming_change)s, %(deprecated)s, %(product_status)s, %(last_updated)s, %(created)s, %(product_status_detail)s)
+INFO:sqlalchemy.engine.Engine:[generated in 0.00019s] {'product_name': 'dfdfwe', 'product_description': '', 'upcoming_change': False, 'deprecated': False, 'product_status': '', 'last_updated': datetime.date(2023, 12, 28), 'created': datetime.date(2023, 12, 28), 'product_status_detail': ''}
 INFO:sqlalchemy.engine.Engine:ROLLBACK
-INFO:werkzeug:127.0.0.1 - - [27/Dec/2023 00:01:44] "POST /opl/add-product HTTP/1.1" 500 -
-Traceback (most recent call last):
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/engine/base.py", line 1969, in _exec_single_context
-    self.dialect.do_execute(
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/engine/default.py", line 922, in do_execute
-    cursor.execute(statement, parameters)
-psycopg2.errors.UndefinedColumn: column "id" of relation "product" does not exist
-LINE 1: INSERT INTO brand_opl.product (id, product_name, product_des...
-                                       ^
-
-
-The above exception was the direct cause of the following exception:
-
+INFO:werkzeug:127.0.0.1 - - [27/Dec/2023 00:08:52] "POST /opl/add-product HTTP/1.1" 500 -
 Traceback (most recent call last):
   File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 1478, in __call__
     return self.wsgi_app(environ, start_response)
@@ -59,41 +48,10 @@ Traceback (most recent call last):
     with util.safe_reraise():
   File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/util/langhelpers.py", line 146, in __exit__
     raise exc_value.with_traceback(exc_tb)
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/orm/session.py", line 4408, in _flush
-    flush_context.execute()
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/orm/unitofwork.py", line 466, in execute
-    rec.execute(self)
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/orm/unitofwork.py", line 642, in execute
-    util.preloaded.orm_persistence.save_obj(
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/orm/persistence.py", line 93, in save_obj
-    _emit_insert_statements(
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/orm/persistence.py", line 1226, in _emit_insert_statements
-    result = connection.execute(
-             ^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/engine/base.py", line 1416, in execute
-    return meth(
-           ^^^^^
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/sql/elements.py", line 516, in _execute_on_connection
-    return connection._execute_clauseelement(
-           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/engine/base.py", line 1639, in _execute_clauseelement
-    ret = self._execute_context(
-          ^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/engine/base.py", line 1848, in _execute_context
-    return self._exec_single_context(
-           ^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/engine/base.py", line 1988, in _exec_single_context
-    self._handle_dbapi_exception(
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/engine/base.py", line 2343, in _handle_dbapi_exception
-    raise sqlalchemy_exception.with_traceback(exc_info[2]) from e
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/engine/base.py", line 1969, in _exec_single_context
-    self.dialect.do_execute(
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/engine/default.py", line 922, in do_execute
-    cursor.execute(statement, parameters)
-sqlalchemy.exc.ProgrammingError: (psycopg2.errors.UndefinedColumn) column "id" of relation "product" does not exist
-LINE 1: INSERT INTO brand_opl.product (id, product_name, product_des...
-                                       ^
-
-[SQL: INSERT INTO brand_opl.product (id, product_name, product_description, upcoming_change, deprecated, product_status, last_updated, created, product_status_detail) VALUES (%(id)s, %(product_name)s, %(product_description)s, %(upcoming_change)s, %(deprecated)s, %(product_status)s, %(last_updated)s, %(created)s, %(product_status_detail)s)]
-[parameters: {'id': 'cc5a9164-48e1-4325-b5dc-54ca784f5a62', 'product_name': 'dfdf', 'product_description': '', 'upcoming_change': False, 'deprecated': False, 'product_status': '', 'last_updated': datetime.date(2023, 12, 28), 'created': datetime.date(2023, 12, 28), 'product_status_detail': ''}]
-(Background on this error at: https://sqlalche.me/e/20/f405)
+  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/orm/session.py", line 4414, in _flush
+    flush_context.finalize_flush_changes()
+  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/orm/unitofwork.py", line 487, in finalize_flush_changes
+    self.session._register_persistent(other)
+  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/orm/session.py", line 3313, in _register_persistent
+    raise exc.FlushError(
+sqlalchemy.orm.exc.FlushError: Instance <Product at 0x7ffbb6d85a60> has a NULL identity key.  If this is an auto-generated value, check that the database table allows generation of new primary key values, and that the mapped Column object is configured to expect these generated values.  Ensure also that this flush() is not occurring at an inappropriate time, such as within a load() event.
