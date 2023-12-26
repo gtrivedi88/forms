e6034812a61a45a283b025bcbde9313551fcfa5b
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2023-12-27 04:57:35 +0530

    Update db.py

--- taxonomy_manager/db.py
+++ taxonomy_manager/db.py
@@ -1,39 +1,88 @@
-from db import db
-import uuid
+from flask import Flask, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
+from forms import MyForm
+from models import db, Product, ProductType, ProductTypeMap, ProductPortfolios, ProductPortfolioMap
 from datetime import datetime
 
+# For troubleshooting
+import logging
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
-class Product(db.Model):
-    __tablename__ = 'product'
-    __table_args__ = {'schema': 'brand_opl'}
-    __uuid__ = "product_id"
-    __term__ = "product_name"
-    __hidden__ = True
-    product_id = db.Column(db.String, primary_key=True, default=str(uuid.uuid4()))
-    product_name = db.Column(db.String(255), nullable=False)
-    product_description = db.Column(db.String)
-    upcoming_change = db.Column(db.Boolean)
-    deprecated = db.Column(db.Boolean)
-    product_status = db.Column(db.String(255))
-    last_updated = db.Column(db.Date)
-    created = db.Column(db.Date)
-    product_status_detail = db.Column(db.String(255))
-    # ccs_data_metadata_product_attributes = db.relationship('ProductAttribute', lazy='subquery', back_populates='brand_opl_product')
-    # brand_opl_product_alias = db.relationship('OplProductAlias', lazy='subquery', back_populates='brand_opl_product')
+app = Flask(__name__)
+<data>
 
-    # Add a relationship to the ProductType model
-    product_type_maps = db.relationship('ProductTypeMap', backref='product', lazy='subquery')
+db.init_app(app)
 
-class ProductType(db.Model):
-    __tablename__ = 'product_types'
-    __table_args__ = {'schema': 'brand_opl'}
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
 
-    type_id = db.Column(db.String, primary_key=True)
-    product_type = db.Column(db.String(255), nullable=False)
+@app.route('/opl/add-product', methods=['GET', 'POST'])
+def add_product():
+    form = MyForm()
+    success_message = None
+    show_form = True
 
-class ProductTypeMap(db.Model):
-    __tablename__ = 'product_types_map'
-    __table_args__ = {'schema': 'brand_opl'}
+    # Initialize the variables here
+    formatted_created_date = None
+    formatted_last_updated_date = None
 
-    product_id = db.Column(db.String, db.ForeignKey('brand_opl.product.product_id'), primary_key=True)
-    type_id = db.Column(db.String, db.ForeignKey('brand_opl.product_types.type_id'), primary_key=True)
+    # Populate product type choices
+    form.product_type.choices = [(ptype.type_id, ptype.product_type) for ptype in ProductType.query.all()]
+
+    # Populate product portfolio choices
+    form.product_portfolio.choices = [(portfolio.category_id, portfolio.category_name) for portfolio in ProductPortfolios.query.all()]
+
+    if form.validate_on_submit():
+        created_date = datetime.now()
+
+        new_product = Product(
+            product_name=form.product_name.data,
+            product_description=form.product_description.data,
+            upcoming_change=form.upcoming_change.data,
+            deprecated=form.deprecated.data,
+            product_status=form.product_status.data,
+            last_updated=created_date,
+            created=created_date,
+            product_status_detail=form.product_status_detail.data
+        )
+        db.session.add(new_product)
+        db.session.commit()
+
+        selected_product_type_id = form.product_type.data
+
+        product_type_map = ProductTypeMap(product_id=new_product.product_id, type_id=selected_product_type_id)
+        db.session.add(product_type_map)
+        db.session.commit()
+
+        selected_portfolio_id = form.product_portfolio.data
+
+        product_portfolio_map = ProductPortfolioMap(product_id=new_product.product_id, category_id=selected_portfolio_id)
+        db.session.add(product_portfolio_map)
+        db.session.commit()
+
+        formatted_created_date = created_date.strftime('%Y-%m-%d')
+        formatted_last_updated_date = created_date.strftime('%Y-%m-%d')
+
+        success_message = f'Successfully added the product: {form.product_name.data}'
+        show_form = False
+
+    return render_template('opl/add.html', form=form, success_message=success_message,
+                           show_form=show_form, formatted_created_date=formatted_created_date,
+                           formatted_last_updated_date=formatted_last_updated_date)
+
+@app.route('/opl/edit-product', methods=['GET', 'POST'])
+def edit_product():
+    form = MyForm()
+
+    if form.validate_on_submit():
+        # Logic for editing the product
+        return render_template('opl/edit.html', form=form, success_message=f'Successfully edited: {form.product_id.data}, {form.product_name.data}')
+
+    return render_template('opl/edit.html', form=form)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
