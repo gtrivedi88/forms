b371b13f9102ea0628241b45f6a184289500dbac
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2023-12-27 14:20:53 +0530

    Update db.py

--- taxonomy_manager/db.py
+++ taxonomy_manager/db.py
@@ -1,10 +1,108 @@
-    <label for="{{ form.product_notes.id }}">{{ form.product_notes.label }}</label>
-    {{ form.product_notes(rows=4, cols=70) }}
+from flask import Flask, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
+from forms import MyForm
+from models import db, Product, ProductType, ProductTypeMap, ProductPortfolios, ProductPortfolioMap, ProductNotes
+from datetime import datetime
 
-sqlalchemy.exc.ProgrammingError: (psycopg2.errors.UndefinedColumn) column product_notes.id does not exist
-LINE 1: ..., p1, sen_counter) ORDER BY sen_counter RETURNING brand_opl....
-                                                             ^
+# For troubleshooting
+import logging
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
-[SQL: INSERT INTO brand_opl.product_notes (product_id, product_note) SELECT p0::VARCHAR, p1::VARCHAR FROM (VALUES (%(product_id__0)s, %(product_note__0)s, 0), (%(product_id__1)s, %(product_note__1)s, 1), (%(product_id__2)s, %(product_note__2)s, 2), (%(product_id__3)s, %(product_note__3)s, 3)) AS imp_sen(p0, p1, sen_counter) ORDER BY sen_counter RETURNING brand_opl.product_notes.id, brand_opl.product_notes.id AS id__1]
-[parameters: {'product_id__0': '94722e16-c442-4af2-9c67-ad2b0bdd8003', 'product_note__0': 'P', 'product_id__1': '94722e16-c442-4af2-9c67-ad2b0bdd8003', 'product_note__1': 'u', 'product_id__2': '94722e16-c442-4af2-9c67-ad2b0bdd8003', 'product_note__2': 'k', 'product_id__3': '94722e16-c442-4af2-9c67-ad2b0bdd8003', 'product_note__3': 'i'}]
-(Background on this error at: https://sqlalche.me/e/20/f405)
+app = Flask(__name__)
+<data>
+
+# Initialize the database
+db.init_app(app)
+
+# Define the index route
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+# Define the route to add a product
+@app.route('/opl/add-product', methods=['GET', 'POST'])
+def add_product():
+    form = MyForm()
+    success_message = None
+    show_form = True
+
+    # Initialize the variables here
+    formatted_created_date = None
+    formatted_last_updated_date = None
+
+    # Populate product type choices
+    form.product_type.choices = [(ptype.type_id, ptype.product_type) for ptype in ProductType.query.all()]
+
+    # Populate product portfolio choices
+    form.product_portfolio.choices = [(portfolio.category_id, portfolio.category_name) for portfolio in ProductPortfolios.query.all()]
+
+    if form.validate_on_submit():
+        # Logic for adding a new product
+        created_date = datetime.now()
+
+        # Create a new product instance
+        new_product = Product(
+            product_name=form.product_name.data,
+            product_description=form.product_description.data,
+            upcoming_change=form.upcoming_change.data,
+            deprecated=form.deprecated.data,
+            product_status=form.product_status.data,
+            last_updated=created_date,
+            created=created_date,
+            product_status_detail=form.product_status_detail.data
+        )
+
+        # Add and commit the new product to the database
+        db.session.add(new_product)
+        db.session.commit()
+
+        # Add product type mapping
+        selected_product_types = form.product_type.data
+        for product_type_id in selected_product_types:
+            product_type_map = ProductTypeMap(product_id=new_product.product_id, type_id=product_type_id)
+            db.session.add(product_type_map)
+
+        # Add product portfolio mapping
+        selected_portfolios = form.product_portfolio.data
+        for portfolio_id in selected_portfolios:
+            product_portfolio_map = ProductPortfolioMap(product_id=new_product.product_id, category_id=portfolio_id)
+            db.session.add(product_portfolio_map)
+        
+        # Add Product Notes
+        selected_notes = form.product_notes.data
+        for note in selected_notes:
+            product_notes = ProductNotes(product_id=new_product.product_id, product_note=note)
+            db.session.add(product_notes)
+        
+        db.session.commit()
+
+        # Format dates for display
+        formatted_created_date = created_date.strftime('%Y-%m-%d')
+        formatted_last_updated_date = created_date.strftime('%Y-%m-%d')
+
+        # Set success message and hide the form
+        success_message = f'Successfully added the product: {form.product_name.data}'
+        show_form = False
+
+    return render_template('opl/add.html', form=form, success_message=success_message,
+                           show_form=show_form, formatted_created_date=formatted_created_date,
+                           formatted_last_updated_date=formatted_last_updated_date)
+
+# Define the route to edit a product
+@app.route('/opl/edit-product', methods=['GET', 'POST'])
+def edit_product():
+    form = MyForm()
+
+    if form.validate_on_submit():
+        # Logic for editing the product
+        return render_template('opl/edit.html', form=form, success_message=f'Successfully edited: {form.product_id.data}, {form.product_name.data}')
+
+    return render_template('opl/edit.html', form=form)
+
+# Run the application if executed directly
+if __name__ == '__main__':
+    with app.app_context():
+        # Create all database tables
+        db.create_all()
+    app.run(debug=True)
