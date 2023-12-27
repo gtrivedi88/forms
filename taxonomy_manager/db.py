979f99513933c829074edef86fc874ae88757210
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2023-12-27 17:49:16 +0530

    Update db.py

--- taxonomy_manager/db.py
+++ taxonomy_manager/db.py
@@ -1,33 +1,329 @@
-Traceback (most recent call last):
-  File "/home/gtrivedi/git/gitlab/opl-ui/app.py", line 4, in <module>
-    from models import db, Product, ProductType, ProductTypeMap, ProductPortfolios, ProductPortfolioMap, ProductNotes, ProductReference
-  File "/home/gtrivedi/git/gitlab/opl-ui/models.py", line 77, in <module>
-    class ProductReference(db.Model):
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask_sqlalchemy/model.py", line 92, in __init__
-    super().__init__(name, bases, d, **kwargs)
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask_sqlalchemy/model.py", line 144, in __init__
-    super().__init__(name, bases, d, **kwargs)
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/orm/decl_api.py", line 195, in __init__
-    _as_declarative(reg, cls, dict_)
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/orm/decl_base.py", line 247, in _as_declarative
-    return _MapperConfig.setup_mapping(registry, cls, dict_, None, {})
-           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/orm/decl_base.py", line 328, in setup_mapping
-    return _ClassScanMapperConfig(
-           ^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/orm/decl_base.py", line 582, in __init__
-    self._early_mapping(mapper_kw)
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/orm/decl_base.py", line 369, in _early_mapping
-    self.map(mapper_kw)
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/orm/decl_base.py", line 1957, in map
-    mapper_cls(self.cls, self.local_table, **self.mapper_args),
-    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "<string>", line 2, in __init__
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/util/deprecations.py", line 281, in warned
-    return fn(*args, **kwargs)  # type: ignore[no-any-return]
-           ^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/orm/mapper.py", line 853, in __init__
-    self._configure_pks()
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/orm/mapper.py", line 1637, in _configure_pks
-    raise sa_exc.ArgumentError(
-sqlalchemy.exc.ArgumentError: Mapper Mapper[ProductReference(product_references)] could not assemble any primary key columns for mapped table 'product_references'
+from flask import Flask, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
+from forms import MyForm
+from models import db, Product, ProductType, ProductTypeMap, ProductPortfolios, ProductPortfolioMap, ProductNotes
+from datetime import datetime
+
+# For troubleshooting
+import logging
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+
+app = Flask(__name__)
+<data>
+
+# Initialize the database
+db.init_app(app)
+
+# Define the index route
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+# Define the route to add a product
+@app.route('/opl/add-product', methods=['GET', 'POST'])
+def add_product():
+    form = MyForm()
+    success_message = None
+    show_form = True
+
+    # Initialize the variables here
+    formatted_created_date = None
+    formatted_last_updated_date = None
+
+    # Populate product type choices
+    form.product_type.choices = [(ptype.type_id, ptype.product_type) for ptype in ProductType.query.all()]
+
+    # Populate product portfolio choices
+    form.product_portfolio.choices = [(portfolio.category_id, portfolio.category_name) for portfolio in ProductPortfolios.query.all()]
+
+    if form.validate_on_submit():
+        # Logic for adding a new product
+        created_date = datetime.now()
+
+        # Create a new product instance
+        new_product = Product(
+            product_name=form.product_name.data,
+            product_description=form.product_description.data,
+            upcoming_change=form.upcoming_change.data,
+            deprecated=form.deprecated.data,
+            product_status=form.product_status.data,
+            last_updated=created_date,
+            created=created_date,
+            product_status_detail=form.product_status_detail.data
+        )
+
+        # Add and commit the new product to the database
+        db.session.add(new_product)
+        db.session.commit()
+
+        # Add product type mapping
+        selected_product_types = form.product_type.data
+        for product_type_id in selected_product_types:
+            product_type_map = ProductTypeMap(product_id=new_product.product_id, type_id=product_type_id)
+            db.session.add(product_type_map)
+
+        # Add product portfolio mapping
+        selected_portfolios = form.product_portfolio.data
+        for portfolio_id in selected_portfolios:
+            product_portfolio_map = ProductPortfolioMap(product_id=new_product.product_id, category_id=portfolio_id)
+            db.session.add(product_portfolio_map)
+        
+        # Add Product Notes
+        selected_notes = form.product_notes.data
+        for note in selected_notes:
+            product_notes = ProductNotes(product_id=new_product.product_id, product_note=note)
+            db.session.add(product_notes)
+        
+        db.session.commit()
+
+        # Format dates for display
+        formatted_created_date = created_date.strftime('%Y-%m-%d')
+        formatted_last_updated_date = created_date.strftime('%Y-%m-%d')
+
+        # Set success message and hide the form
+        success_message = f'Successfully added the product: {form.product_name.data}'
+        show_form = False
+
+    return render_template('opl/add.html', form=form, success_message=success_message,
+                           show_form=show_form, formatted_created_date=formatted_created_date,
+                           formatted_last_updated_date=formatted_last_updated_date)
+
+# Define the route to edit a product
+@app.route('/opl/edit-product', methods=['GET', 'POST'])
+def edit_product():
+    form = MyForm()
+
+    if form.validate_on_submit():
+        # Logic for editing the product
+        return render_template('opl/edit.html', form=form, success_message=f'Successfully edited: {form.product_id.data}, {form.product_name.data}')
+
+    return render_template('opl/edit.html', form=form)
+
+# Run the application if executed directly
+if __name__ == '__main__':
+    with app.app_context():
+        # Create all database tables
+        db.create_all()
+    app.run(debug=True)
+
+
+
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField, SelectMultipleField
+
+# Import DateField is duplicated, removed the second import
+# from wtforms import DateField
+
+# Below are the form classes for each taxonomy. Each is based on the wtforms
+# specification. However, there are some additional variables to help with a
+# few things:
+#
+#   form_choices        Used with forms that contain SelectField and
+#                       SelectMultipleField. In some cases, the choices option
+#                       for these two field types are usually taken from another
+#                       table, which means they need to be set within the
+#                       context of the Flask app. So this variable sets a
+#                       mapping to data from another table, which the
+#                       utils.get_choices_for_selectfields method renders and
+#                       adds to the choices option of the relevant field. The
+#                       mapping is as follows:
+#                           'model'     The name of the model to pull data from.
+#                           'value'     The column to use for the select field value
+#                           'label'     The column to use for the select field label
+#   mapped_data         This variable acts as a trigger to let the Flask app
+#                       know that secondary table mapping is used. This is used
+#                       to both get data and set data to the secondary table.
+
+class MyForm(FlaskForm):
+    product_name = StringField('Product Name *', validators=[DataRequired()])
+    product_description = TextAreaField('Product Description')
+    upcoming_change = BooleanField('Upcoming Change')
+    deprecated = BooleanField('Deprecated')
+    product_status = StringField('Product Status')
+    product_status_detail = TextAreaField('Product Status Detail')
+
+    # SelectField for Product Status
+    product_status = SelectField('Product Status', choices=[('deprecated', 'Deprecated'), ('upcoming', 'Upcoming'), ('available', 'Available')])
+
+    # SelectField for Product Type
+    product_type = SelectMultipleField('Product Type', validators=[DataRequired()])
+
+    # SelectField for Product Portfolio
+    product_portfolio = SelectMultipleField('Portfolio')
+
+    # Field for Product Notes
+    product_notes = TextAreaField('Product Notes')
+
+    submit = SubmitField('Add Product')
+
+
+
+
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+
+db = SQLAlchemy()
+
+class Product(db.Model):
+    __tablename__ = 'product'
+    __table_args__ = {'schema': 'brand_opl'}
+    __uuid__ = "product_id"
+    __term__ = "product_name"
+    __hidden__ = True
+
+    # ProductPortfolios fields
+    product_id = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
+    product_name = db.Column(db.String(255), nullable=False)
+    product_description = db.Column(db.String)
+    upcoming_change = db.Column(db.Boolean)
+    deprecated = db.Column(db.Boolean)
+    product_status = db.Column(db.String(255))
+    last_updated = db.Column(db.Date)
+    created = db.Column(db.Date)
+    product_status_detail = db.Column(db.String(255))
+
+    # Relationships
+    product_type_maps = db.relationship('ProductTypeMap', backref='product', lazy='dynamic')
+    product_portfolios = db.relationship('ProductPortfolioMap', backref='product', lazy='dynamic')
+    product_notes = db.relationship('ProductNotes', backref='product', lazy='dynamic')
+
+class ProductType(db.Model):
+    __tablename__ = 'product_types'
+    __table_args__ = {'schema': 'brand_opl'}
+
+    # ProductType fields
+    type_id = db.Column(db.String, primary_key=True)
+    product_type = db.Column(db.String(255), nullable=False)
+
+class ProductTypeMap(db.Model):
+    __tablename__ = 'product_types_map'
+    __table_args__ = {'schema': 'brand_opl'}
+
+    # ProductTypeMap fields    
+    product_id = db.Column(db.String, db.ForeignKey('brand_opl.product.product_id'), primary_key=True)
+    type_id = db.Column(db.String, db.ForeignKey('brand_opl.product_types.type_id'), primary_key=True)
+
+class ProductPortfolios(db.Model):
+    __tablename__ = 'product_portfolios'
+    __table_args__ = {'schema': 'brand_opl'}
+    __uuid__ = "category_id"
+    __term__ = "category_name"
+
+    # ProductPortfolios fields
+    category_id = db.Column(db.String, primary_key=True)
+    category_name = db.Column(db.String(255), nullable=False)
+
+class ProductPortfolioMap(db.Model):
+    __tablename__ = 'product_portfolio_map'
+    __table_args__ = {'schema': 'brand_opl'}
+    __uuid__ = "product_id"
+    __term__ = "category_id"
+
+    # ProductPortfolioMap fields
+    product_id = db.Column(db.String, db.ForeignKey('brand_opl.product.product_id'), primary_key=True)
+    category_id = db.Column(db.String, db.ForeignKey('brand_opl.product_portfolios.category_id'), primary_key=True)
+
+class ProductNotes(db.Model):
+    __tablename__ = 'product_notes'
+    __table_args__ = {'schema': 'brand_opl'}
+    __uuid__ = "product_id"
+    __term__ = "product_note"
+
+    # ProductNotes fields
+    product_id = db.Column(db.String, db.ForeignKey('brand_opl.product.product_id'))
+    product_note = db.Column(db.String(65535), db.ForeignKey('brand_opl.product_notes.product_note'), primary_key=True)
+
+
+
+
+{% extends 'base.html' %}
+
+{% block heading %}
+<h1 class="pf-v5-c-title pf-m-4xl">Welcome to OPL</h1>
+{% endblock %}
+
+{% block content %}
+<p>Add a product information.</p>
+
+<br><br>
+
+{% if show_form %}
+
+<p style="color:red;">Fields marked with * are mandatory.</p>
+
+<form method="post" action="{{ url_for('add_product') }}">
+    {{ form.hidden_tag() }}
+
+    <label for="{{ form.product_name.id }}">{{ form.product_name.label }}</label>
+    {{ form.product_name() }}
+
+    <br><br>
+
+    <label for="{{ form.product_description.id }}">{{ form.product_description.label }}</label>
+    {{ form.product_description(rows=4, cols=50) }}
+
+    <br><br>
+
+    <label for="{{ form.product_portfolio.id }}">{{ form.product_portfolio.label }}</label>
+    <select id="{{ form.product_portfolio.id }}" name="{{ form.product_portfolio.name }}" multiple>
+        {% for value, label in form.product_portfolio.choices %}
+        <option value="{{ value }}">{{ label }}</option>
+        {% endfor %}
+    </select>
+
+    <br><br>
+
+    <label for="{{ form.product_notes.id }}">{{ form.product_notes.label }}</label>
+    {{ form.product_notes() }}
+
+    <br><br>
+
+    <label for="{{ form.product_type.id }}">{{ form.product_type.label }}</label>
+    <select id="{{ form.product_type.id }}" name="{{ form.product_type.name }}" multiple>
+        {% for value, label in form.product_type.choices %}
+        <option value="{{ value }}">{{ label }}</option>
+        {% endfor %}
+    </select>
+
+    <br><br>
+
+    <div class="checkbox-row">
+        {{ form.deprecated() }}
+        <label for="{{ form.deprecated.id }}">{{ form.deprecated.label }}</label>
+    </div>
+
+    <br><br>
+
+    <div class="checkbox-row">
+        {{ form.upcoming_change() }}
+        <label for="{{ form.upcoming_change.id }}">{{ form.upcoming_change.label }}</label>
+    </div>
+
+    <br><br>
+
+    <label for="{{ form.product_status.id }}">{{ form.product_status.label }}</label>
+    <select id="{{ form.product_status.id }}" name="{{ form.product_status.name }}">
+        {% for value, label in form.product_status.choices %}
+        <option value="{{ value }}">{{ label }}</option>
+        {% endfor %}
+    </select>
+
+    <br><br>
+
+    <label for="{{ form.product_status_detail.id }}">{{ form.product_status_detail.label }}</label>
+    {{ form.product_status_detail(rows=4, cols=50) }}
+
+    <br><br>
+
+    {{ form.submit(style="background-color: #1a73e8; color: #ffffff;") }}
+</form>
+{% endif %}
+
+{% if success_message %}
+<p class="success">{{ success_message }}</p>
+<br><br>
+<a href="{{ url_for('add_product') }}" class="button-link">Add more products</a>
+{% endif %}
+{% endblock %}
