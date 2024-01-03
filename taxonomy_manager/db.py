1a9ec9b978d1f3a53dddc847782039d225e676de
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-01-03 13:37:21 +0530

    Update db.py

--- taxonomy_manager/db.py
+++ taxonomy_manager/db.py
@@ -1,43 +1,15 @@
-apiVersion: tekton.dev/v1
-kind: Task
-metadata:
-  name: vulnerability-scan <.>
-  annotations:
-	task.output.location: results <.>
-	task.results.format: application/json
-	task.results.key: SCAN_OUTPUT
-spec:
-  results:
-	- description: CVE result format <.>
-  	  name: SCAN_OUTPUT
-  	  type: string
-  steps:
-	- name: scan <.>
-  	  image: quay.io/container.image <.> 
-  	  env:
-    	     - name: TOOL_ENV_VAR1  <.>
-      	       valueFrom:
-        	           secretKeyRef:
-          	               key: tool_var1
-          	               name: tool-secrets 
-  	script: | <.>
-    	
-#!/bin/sh
-    	# Tool-specific setup 
-    	
-jq -rce \ <.>
-    	"{vulnerabilities:{
-    	critical: (.result.summary.CRITICAL),
-    	high: (.result.summary.IMPORTANT),
-    	medium: (.result.summary.MODERATE),
-    	low: (.result.summary.LOW)
-    	}}" scan_output.json | tee $(results.SCAN_OUTPUT.path)
+Enhancing OpenShift Pipelines with Software Supply Chain Security Use Cases
+Software supply chain security ensures the integrity of and security of your application throughout their development lifecycle. 
 
-<.>  The name of your task.
-<.>  The location for storing the task outputs
-<.>  The description of the result.
-<.> The name of the vulnerability scanning tool that you have used. For example, you can use Roxctl. 
-<.> The location of the actual image containing the scan tool.
-<.> The tool-specific environment variables
-<.>     Set up the shell script to be executed. You must ensure that the scan results are outputted in the json format. For example, scan_output.json.
-<.>  Extract vulnerability summary (adjust jq command for different JSON structures).
+
+Openshift Pipelines strengthens its commitment to software supply chain security, offering enhanced features to streamline vulnerabilities management and transparency
+
+
+The PipelineRun Details view in the Developer or Administrator perspective of the web console provides an enhanced visual representation of PipelinesRuns within a project. 
+
+For a PipelineRun Details, you can now view:
+
+A signed badge indicating the security and provenance of the PipelineRun
+An Output tab to view the PipelineRun results
+Vulnerabilities within a project
+Link to view and download Software bill of materials (SBOMs)
