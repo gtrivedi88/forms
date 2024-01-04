df9c5e10026bfc6f2b674854c0ba0f9f4fa6a0e6
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-01-04 16:25:51 +0530

    Update db.py

--- taxonomy_manager/db.py
+++ taxonomy_manager/db.py
@@ -1,15 +1,158 @@
-Enhancing OpenShift Pipelines with Software Supply Chain Security Use Cases
-Software supply chain security ensures the integrity of and security of your application throughout their development lifecycle. 
+class ProductReferences(db.Model):
+    """
+    Represents references associated with a product.
+
+    Attributes:
+    - product_id: Foreign key to Product.
+    - product_link: Link associated with the product reference.
+    - link_description: Description associated with the product reference.
+
+    Relationships:
+    - product: Relationship to Product for easy access to the associated product references.
+    """
+
+    __tablename__ = 'product_references'
+    __table_args__ = {'schema': 'brand_opl'}
+    __uuid__ = "product_id"
+    __term__ = "product_link"
+
+    product_id = db.Column(db.String, db.ForeignKey('brand_opl.product.product_id'), primary_key=True)
+    product_link = db.Column(db.String(65535), nullable=False)
+    link_description = db.Column(db.String(65535), nullable=False)
+
+    # Define the relationship to the Product model
+    product = db.relationship('Product', backref=db.backref('product_references', lazy='dynamic'))
 
 
-Openshift Pipelines strengthens its commitment to software supply chain security, offering enhanced features to streamline vulnerabilities management and transparency
 
 
-The PipelineRun Details view in the Developer or Administrator perspective of the web console provides an enhanced visual representation of PipelinesRuns within a project. 
 
-For a PipelineRun Details, you can now view:
+    # Field for Product References
+    product_link = TextAreaField('Product Reference')
+    link_description = TextAreaField('Reference Description')
 
-A signed badge indicating the security and provenance of the PipelineRun
-An Output tab to view the PipelineRun results
-Vulnerabilities within a project
-Link to view and download Software bill of materials (SBOMs)
+
+
+
+from flask import Flask, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
+from forms import MyForm
+from models import db, Product, ProductType, ProductTypeMap, ProductPortfolios, ProductPortfolioMap, ProductNotes, ProductReferences
+from datetime import datetime
+
+# For troubleshooting
+import logging
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+
+app = Flask(__name__)
+<data>
+# Initialize the database
+db.init_app(app)
+
+# Define the index route
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+# Define the route to add a product
+@app.route('/opl/add-product', methods=['GET', 'POST'])
+def add_product():
+    form = MyForm()
+    success_message = None
+    show_form = True
+
+    # Initialize the variables here
+    formatted_created_date = None
+    formatted_last_updated_date = None
+
+    # Populate product type choices
+    form.product_type.choices = [(ptype.type_id, ptype.product_type) for ptype in ProductType.query.all()]
+
+    # Populate product portfolio choices
+    form.product_portfolio.choices = [(portfolio.category_id, portfolio.category_name) for portfolio in ProductPortfolios.query.all()]
+
+    # Add Product References
+    product_references_data = {
+        'product_link': form.product_link.data,
+        'link_description': form.link_description.data
+        }
+
+    if form.validate_on_submit():
+        # Logic for adding a new product
+        created_date = datetime.now()
+
+        # Create a new product instance
+        new_product = Product(
+            product_name=form.product_name.data,
+            product_description=form.product_description.data,
+            upcoming_change=form.upcoming_change.data,
+            deprecated=form.deprecated.data,
+            product_status=form.product_status.data,
+            last_updated=created_date,
+            created=created_date,
+            product_status_detail=form.product_status_detail.data
+        )
+
+        # Add and commit the new product to the database
+        db.session.add(new_product)
+        db.session.commit()
+
+        # Add product type mapping
+        selected_product_types = form.product_type.data
+        for product_type_id in selected_product_types:
+            product_type_map = ProductTypeMap(product_id=new_product.product_id, type_id=product_type_id)
+            db.session.add(product_type_map)
+
+        # Add product portfolio mapping
+        selected_portfolios = form.product_portfolio.data
+        for portfolio_id in selected_portfolios:
+            product_portfolio_map = ProductPortfolioMap(product_id=new_product.product_id, category_id=portfolio_id)
+            db.session.add(product_portfolio_map)
+        
+        # Add Product Notes
+        product_notes_data = form.product_notes.data
+        if product_notes_data:
+            product_notes = ProductNotes(product_id=new_product.product_id, product_note=product_notes_data)
+            db.session.add(product_notes)
+
+        # Add Product References and Description
+        if any(product_references_data.values()):
+            product_references = ProductReferences(
+                product_id=new_product.product_id,
+                product_link=product_references_data['product_link'],
+                link_description=product_references_data['link_description']
+            )
+            db.session.add(product_references)
+        
+        db.session.commit()
+
+        # Format dates for display
+        formatted_created_date = created_date.strftime('%Y-%m-%d')
+        formatted_last_updated_date = created_date.strftime('%Y-%m-%d')
+
+        # Set success message and hide the form
+        success_message = f'Successfully added the product: {form.product_name.data}'
+        show_form = False
+
+    return render_template('opl/add.html', form=form, success_message=success_message,
+                           show_form=show_form, formatted_created_date=formatted_created_date,
+                           formatted_last_updated_date=formatted_last_updated_date)
+
+# Define the route to edit a product
+@app.route('/opl/edit-product', methods=['GET', 'POST'])
+def edit_product():
+    form = MyForm()
+
+    if form.validate_on_submit():
+        # Logic for editing the product
+        return render_template('opl/edit.html', form=form, success_message=f'Successfully edited: {form.product_id.data}, {form.product_name.data}')
+
+    return render_template('opl/edit.html', form=form)
+
+# Run the application if executed directly
+if __name__ == '__main__':
+    with app.app_context():
+        # Create all database tables
+        db.create_all()
+    app.run(debug=True)
