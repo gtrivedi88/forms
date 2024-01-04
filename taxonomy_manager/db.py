eb83204fdf2e69ddec53ddb1b34ce94a5a5fd324
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-01-05 03:15:28 +0530

    Update db.py

--- taxonomy_manager/db.py
+++ taxonomy_manager/db.py
@@ -7,12 +7,20 @@
             alias_name = request.form.getlist('alias_name')[i]
             alias_type = request.form.getlist('alias_type')[i]
             alias_approved = request.form.getlist('alias_approved')[i]
+            previous_name = request.form.getlist('previous_name')[i]
+            tech_docs = request.form.getlist('tech_docs')[i]
+            tech_docs_cli = request.form.getlist('tech_docs_cli')[i]
+            alias_notes = request.form.getlist('alias_notes')[i]
 
             if alias_name and alias_type:
                 product_alias_data.append({
                     'alias_name': alias_name,
                     'alias_type': alias_type,
-                    'alias_approved': alias_approved
+                    'alias_approved': alias_approved,
+                    'previous_name': previous_name,
+                    'tech_docs': tech_docs,
+                    'tech_docs_cli': tech_docs_cli,
+                    'alias_notes': alias_notes
                 })
 
         # Print or log product_alias_data for debugging
@@ -20,17 +28,30 @@
 
         # Loop to save aliases
         for alias_data in product_alias_data:
+            # Check if 'alias_approved' key exists in alias_data
+            if 'alias_approved' in alias_data:
+                alias_approved = alias_data['alias_approved'].lower() == 'y'
+                previous_name = alias_data['previous_name'].lower() == 'y'
+                tech_docs = alias_data['tech_docs'].lower() == 'y'
+                tech_docs_cli = alias_data['tech_docs_cli'].lower() == 'y'
+            else:
+                # If the key is not present, provide a default value (False in this case)
+                alias_approved = False
+                previous_name = False
+                tech_docs = False
+                tech_docs_cli = False
+
             product_alias = ProductAlias(
                 product_id=new_product.product_id,
                 alias_name=alias_data['alias_name'],
                 alias_type=alias_data['alias_type'],
-                alias_approved=alias_data['alias_approved']
+                alias_approved=alias_approved,
+                previous_name=previous_name,
+                tech_docs=tech_docs,
+                tech_docs_cli=tech_docs_cli,
+                alias_notes=alias_data['alias_notes']
             )
             db.session.add(product_alias)
 
-
-
-
-sqlalchemy.exc.StatementError: (builtins.TypeError) Not a boolean value: 'y'
-[SQL: INSERT INTO brand_opl.product_alias (alias_id, product_id, alias_name, alias_type, alias_approved, previous_name, tech_docs, tech_docs_cli, alias_notes) VALUES (%(alias_id)s, %(product_id)s, %(alias_name)s, %(alias_type)s, %(alias_approved)s, %(previous_name)s, %(tech_docs)s, %(tech_docs_cli)s, %(alias_notes)s)]
-[parameters: [{'product_id': '8bce8b1a-ecf5-47b1-a25a-14761d88e15f', 'alias_name': 'werty', 'alias_type': 'Acronym', 'alias_approved': 'y', 'previous_name': None, 'alias_notes': None, 'tech_docs': None, 'tech_docs_cli': None}, {'product_id': '8bce8b1a-ecf5-47b1-a25a-14761d88e15f', 'alias_name': 'asas', 'alias_type': 'Short', 'alias_approved': '', 'previous_name': None, 'alias_notes': None, 'tech_docs': None, 'tech_docs_cli': None}]]
+        # Commit changes to the database session
+        db.session.commit()
