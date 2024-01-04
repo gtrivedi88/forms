b605648c0bc4a81230dcacc2ab675e8769d75af5
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-01-04 20:09:29 +0530

    Update db.py

--- taxonomy_manager/db.py
+++ taxonomy_manager/db.py
@@ -1,80 +1,176 @@
-label {
-    display: block;
-    margin-bottom: 5px;
-    font-weight: bold;
-}
+{% extends 'base.html' %}
 
-input,
-textarea,
-select {
-    padding: 5px;
-    margin-bottom: 10px;
-}
+{% block heading %}
+<h1 class="pf-v5-c-title pf-m-4xl">Welcome to OPL</h1>
+{% endblock %}
 
-button {
-    padding: 10px;
-    background-color: #4CAF50;
-    color: white;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-}
+{% block content %}
+<p>Add a product information.</p>
 
-button:hover {
-    background-color: #45a049;
-}
+<br><br>
 
-.checkbox-row {
-    display: flex;
-    align-items: center;
-}
+{% if show_form %}
 
-.checkbox-row input {
-    margin-right: 10px;
-}
+<p style="color:red;">Fields marked with * are mandatory.</p>
 
-.button-link {
-    display: inline-block;
-    padding: 10px 20px;
-    background-color: #1a73e8;
-    color: white;
-    text-decoration: none;
-    border-radius: 12px;
-}
+<form method="post" action="{{ url_for('add_product') }}">
+    {{ form.hidden_tag() }}
 
-.success {
-    color: green;
-    font-weight: bold;
-}
+    <label for="{{ form.product_name.id }}">{{ form.product_name.label }}</label>
+    {{ form.product_name(row=4, cols=40) }}
 
-.product-reference-pair {
-    display: flex;
-}
+    <br><br>
 
-.form-group {
-    display: flex;
-}
+    <label for="{{ form.product_description.id }}">{{ form.product_description.label }}</label>
+    {{ form.product_description(cols=40) }}
 
-.form-field {
-    flex: auto;
-    /* Adjust the width as needed */
-}
+    <br><br>
 
-.checkbox-field {
-    display: flex;
-    flex: 1;
-}
+    <label for="{{ form.product_portfolio.id }}">{{ form.product_portfolio.label }}</label>
+    <select id="{{ form.product_portfolio.id }}" name="{{ form.product_portfolio.name }}" multiple>
+        {% for value, label in form.product_portfolio.choices %}
+        <option value="{{ value }}">{{ label }}</option>
+        {% endfor %}
+    </select>
 
-.checkbox-field input {
-    margin-right: 10px;
-    /* Adjust the space between the checkbox and label */
-}
+    <br><br>
 
-.buttons-row {
-    align-items: center;
-    margin-top: 35px;
-}
+    <label for="{{ form.product_notes.id }}">{{ form.product_notes.label }}</label>
+    {{ form.product_notes() }}
 
-.buttons-row button {
-    margin-left: 10px;
-}
+    <br><br>
+
+    <label for="{{ form.product_type.id }}">{{ form.product_type.label }}</label>
+    <select id="{{ form.product_type.id }}" name="{{ form.product_type.name }}" multiple>
+        {% for value, label in form.product_type.choices %}
+        <option value="{{ value }}">{{ label }}</option>
+        {% endfor %}
+    </select>
+
+    <br><br>
+
+    <div id="product-references">
+        <div class="product-reference-pair">
+            <div class="form-field">
+                <label for="{{ form.product_link.id }}">Product Reference</label>
+                {{ form.product_link(cols=40) }}
+            </div>
+    
+            <div class="form-field">
+                <label for="{{ form.link_description.id }}">Reference Description</label>
+                {{ form.link_description(cols=40) }}
+            </div>
+    
+            <div class="form-field buttons-row">
+                <button type="button" class="add-reference">Add more</button>
+                <button type="button" class="remove-reference" style="display: none;">Delete</button>
+            </div>
+        </div>
+    </div>
+
+
+    <br><br>
+
+    <div class="form-group">
+        <div class="checkbox-field">
+            {{ form.deprecated() }}
+            <label for="{{ form.deprecated.id }}">{{ form.deprecated.label }}</label>
+        </div>
+    
+        <div class="checkbox-field">
+            {{ form.upcoming_change() }}
+            <label for="{{ form.upcoming_change.id }}">{{ form.upcoming_change.label }}</label>
+        </div>
+    </div>
+
+    <br><br>
+
+    <div class="form-group">
+        <div class="form-field">
+            <label for="{{ form.product_status.id }}">Status</label>
+            {{ form.product_status(id="status-dropdown", class="status-dropdown") }}
+        </div>
+
+        <div class="form-field">
+            <label for="{{ form.product_status_detail.id }}">Status details</label>
+            {{ form.product_status_detail(id="status-details-dropdown", class="status-details-dropdown") }}
+        </div>
+    </div>
+
+    <br><br>
+
+    <!-- Product Alias Information Group -->
+    <fieldset class="product-alias-group">
+        <legend>Product Alias Information</legend>
+
+        <!-- Fields for Product Alias -->
+        <div class="form-group">
+            <label for="{{ form.alias_name.id }}">{{ form.alias_name.label }}</label>
+            {{ form.alias_name() }}
+        </div>
+
+        <br>
+
+        <label for="{{ form.alias_type.id }}">{{ form.alias_type.label }}</label>
+        {{ form.alias_type() }}
+
+        <br><br>
+
+        <!-- Fields for Product Alias -->
+        <div class="form-group">
+            <div class="checkbox-field">
+                {{ form.alias_approved() }}
+                <label for="{{ form.alias_approved.id }}" data-toggle="tooltip"
+                    title="Leaving it blank for unapproved aliases.">{{ form.alias_approved.label }}</label>
+            </div>
+
+            <div class="checkbox-field">
+                {{ form.previous_name() }}
+                <label for="{{ form.previous_name.id }}">{{ form.previous_name.label }}</label>
+            </div>
+
+            <div class="checkbox-field">
+                {{ form.tech_docs() }}
+                <label for="{{ form.tech_docs.id }}">{{ form.tech_docs.label }}</label>
+            </div>
+
+            <div class="checkbox-field">
+                {{ form.tech_docs_cli() }}
+                <label for="{{ form.tech_docs_cli.id }}">{{ form.tech_docs_cli.label }}</label>
+            </div>
+        </div>
+
+        <br>
+
+        <label for="{{ form.alias_notes.id }}">{{ form.alias_notes.label }}</label>
+        {{ form.alias_notes(cols=40) }}
+
+    <br><br>
+    <!-- "Delete" button for the current group -->
+    <button type="button" class="delete-group-btn" onclick="deleteGroup(this)">Delete Group</button>
+    
+    </fieldset>
+    
+    <br><br>
+
+        <!-- "Add" button to add more groups -->
+        <button type="button" id="add-group-btn" onclick="addGroup()">Add Another Alias Group</button>
+
+    <br><br>
+
+    {{ form.submit(style="background-color: #1a73e8; color: #ffffff;") }}
+</form>
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="{{ url_for('static', filename='scripts/status.js') }}"></script>
+<script src="{{ url_for('static', filename='scripts/preferences.js') }}"></script>
+<script src="{{ url_for('static', filename='scripts/alias.js') }}"></script>
+
+{% endif %}
+
+{% if success_message %}
+<p class="success">{{ success_message }}</p>
+<br><br>
+<a href="{{ url_for('add_product') }}" class="button-link">Add more products</a>
+{% endif %}
+{% endblock %}
