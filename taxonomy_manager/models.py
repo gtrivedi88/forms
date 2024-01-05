54ccec26dcbe17bfb15935a691d4136844379c1d
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-01-05 17:16:06 +0530

    Update models.py

--- taxonomy_manager/models.py
+++ taxonomy_manager/models.py
@@ -19,36 +19,47 @@
     <fieldset class="product-information-group">
     <legend>Product Information</legend>
 
-    <label for="{{ form.product_name.id }}">{{ form.product_name.label }}</label>
-    {{ form.product_name(row=4, cols=40) }}
+        <div class="form-group">
+            <div class="form-field">
+                <label for="{{ form.product_name.id }}">{{ form.product_name.label }}</label>
+                {{ form.product_name(cols=40) }}
+            </div>
 
-    <br><br>
 
-    <label for="{{ form.product_description.id }}">{{ form.product_description.label }}</label>
-    {{ form.product_description(cols=40) }}
+            <div class="form-field">
+                <label for="{{ form.product_type.id }}">{{ form.product_type.label }}</label>
+                <select id="{{ form.product_type.id }}" name="{{ form.product_type.name }}" multiple>
+                    {% for value, label in form.product_type.choices %}
+                    <option value="{{ value }}">{{ label }}</option>
+                    {% endfor %}
+                </select>
+            </div>
+        </div>
 
-    <br><br>
 
-    <label for="{{ form.product_portfolio.id }}">{{ form.product_portfolio.label }}</label>
-    <select id="{{ form.product_portfolio.id }}" name="{{ form.product_portfolio.name }}" multiple>
-        {% for value, label in form.product_portfolio.choices %}
-        <option value="{{ value }}">{{ label }}</option>
-        {% endfor %}
-    </select>
+        <div class="form-group">
+            
+            <div class="form-field">
+                <label for="{{ form.product_description.id }}">{{ form.product_description.label }}</label>
+                {{ form.product_description(cols=40) }}
+            </div>
 
-    <br><br>
+            <div class="form-field">
+                <label for="{{ form.product_portfolio.id }}">{{ form.product_portfolio.label }}</label>
+                <select id="{{ form.product_portfolio.id }}" name="{{ form.product_portfolio.name }}" multiple>
+                    {% for value, label in form.product_portfolio.choices %}
+                    <option value="{{ value }}">{{ label }}</option>
+                    {% endfor %}
+                </select>
+            </div>
+        </div>
 
-    <label for="{{ form.product_notes.id }}">{{ form.product_notes.label }}</label>
-    {{ form.product_notes() }}
-
-    <br><br>
-
-    <label for="{{ form.product_type.id }}">{{ form.product_type.label }}</label>
-    <select id="{{ form.product_type.id }}" name="{{ form.product_type.name }}" multiple>
-        {% for value, label in form.product_type.choices %}
-        <option value="{{ value }}">{{ label }}</option>
-        {% endfor %}
-    </select>
+        <div class="form-group">
+            <div class="form-field">
+                <label for="{{ form.product_notes.id }}">{{ form.product_notes.label }}</label>
+                {{ form.product_notes(cols=40) }}
+            </div>
+        </div>
 
     </fieldset>
 
