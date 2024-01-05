572646bfad3f9758e3f7fb75b9981017ee4deee8
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-01-06 04:18:19 +0530

    Update forms.py

--- taxonomy_manager/forms.py
+++ taxonomy_manager/forms.py
@@ -1,15 +1,19 @@
-$(document).ready(function () {
-        // Add more Product Reference and Reference Description pairs
-        $(".add-reference").click(function () {
-            var referencePair = $(".product-reference-pair:first").clone();
-            referencePair.find("input, textarea").val('');
-            referencePair.find(".add-reference").hide();
-            referencePair.find(".remove-reference").show();
-            $("#product-references").append(referencePair);
-        });
+# Add Product References
+        product_references_data = []
+        for i in range(len(request.form.getlist('product_link'))):
+            product_link = request.form.getlist('product_link')[i]
+            link_description = request.form.getlist('link_description')[i]
 
-        // Remove the current Product Reference and Reference Description pair
-        $(document).on("click", ".remove-reference", function () {
-            $(this).closest(".product-reference-pair").remove();
-        });
-    });
+            if product_link and link_description:
+                product_references_data.append({
+                    'product_link': product_link,
+                    'link_description': link_description
+                })
+
+        for reference_data in product_references_data:
+            product_references = ProductReferences(
+                product_id=new_product.product_id,
+                product_link=reference_data['product_link'],
+                link_description=reference_data['link_description']
+            )
+            db.session.add(product_references)
