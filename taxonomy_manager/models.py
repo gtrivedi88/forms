a28de4888f5502bd46b1faba9df6d2d06fce5b55
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-01-05 16:54:57 +0530

    Update models.py

--- taxonomy_manager/models.py
+++ taxonomy_manager/models.py
@@ -1,195 +1,207 @@
-from flask import Flask, render_template, flash, request
-from flask_sqlalchemy import SQLAlchemy
-from forms import MyForm
-from models import db, Product, ProductType, ProductTypeMap, ProductPortfolios, ProductPortfolioMap, ProductNotes, ProductReferences, ProductAlias
-from datetime import datetime
+{% extends 'base.html' %}
 
-# For troubleshooting
-import logging
-logging.basicConfig()
-logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+{% block heading %}
+<h1 class="pf-v5-c-title pf-m-4xl">Welcome to OPL</h1>
+{% endblock %}
 
-app = Flask(__name__)
-<data>
+{% block content %}
+<p>Add a product information.</p>
 
-# Initialize the database
-db.init_app(app)
+<br><br>
 
-# Define the index route
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
+{% if show_form %}
 
-# Define the route to add a product
-@app.route('/opl/add-product', methods=['GET', 'POST'])
-def add_product():
-    form = MyForm()
-    success_message = None
-    show_form = True
+<p style="color:red;">Fields marked with * are mandatory.</p>
 
-    # Initialize the variables here
-    formatted_created_date = None
-    formatted_last_updated_date = None
+<form method="post" action="{{ url_for('add_product') }}">
+    {{ form.hidden_tag() }}
 
-    # Populate product type choices
-    form.product_type.choices = [(ptype.type_id, ptype.product_type) for ptype in ProductType.query.all()]
+    <fieldset class="product-information-group">
+    <legend>Product Information</legend>
 
-    # Populate product portfolio choices
-    form.product_portfolio.choices = [(portfolio.category_id, portfolio.category_name) for portfolio in ProductPortfolios.query.all()]
+    <label for="{{ form.product_name.id }}">{{ form.product_name.label }}</label>
+    {{ form.product_name(row=4, cols=40) }}
 
+    <br><br>
 
-    if form.validate_on_submit():
-        # Logic for adding a new product
-        created_date = datetime.now()
+    <label for="{{ form.product_description.id }}">{{ form.product_description.label }}</label>
+    {{ form.product_description(cols=40) }}
 
-        # Create a new product instance
-        new_product = Product(
-            product_name=form.product_name.data,
-            product_description=form.product_description.data,
-            upcoming_change=form.upcoming_change.data,
-            deprecated=form.deprecated.data,
-            product_status=form.product_status.data,
-            last_updated=created_date,
-            created=created_date,
-            product_status_detail=form.product_status_detail.data
-        )
+    <br><br>
 
-        # Add and commit the new product to the database
-        db.session.add(new_product)
-        db.session.commit()
+    <label for="{{ form.product_portfolio.id }}">{{ form.product_portfolio.label }}</label>
+    <select id="{{ form.product_portfolio.id }}" name="{{ form.product_portfolio.name }}" multiple>
+        {% for value, label in form.product_portfolio.choices %}
+        <option value="{{ value }}">{{ label }}</option>
+        {% endfor %}
+    </select>
 
-        # Add product type mapping
-        selected_product_types = form.product_type.data
-        for product_type_id in selected_product_types:
-            product_type_map = ProductTypeMap(product_id=new_product.product_id, type_id=product_type_id)
-            db.session.add(product_type_map)
+    <br><br>
 
-        # Add product portfolio mapping
-        selected_portfolios = form.product_portfolio.data
-        for portfolio_id in selected_portfolios:
-            product_portfolio_map = ProductPortfolioMap(product_id=new_product.product_id, category_id=portfolio_id)
-            db.session.add(product_portfolio_map)
+    <label for="{{ form.product_notes.id }}">{{ form.product_notes.label }}</label>
+    {{ form.product_notes() }}
+
+    <br><br>
+
+    <label for="{{ form.product_type.id }}">{{ form.product_type.label }}</label>
+    <select id="{{ form.product_type.id }}" name="{{ form.product_type.name }}" multiple>
+        {% for value, label in form.product_type.choices %}
+        <option value="{{ value }}">{{ label }}</option>
+        {% endfor %}
+    </select>
+
+    </fieldset>
+
+    <br><br>
+    
+    <fieldset class="product-reference-group">
+    <legend>Product Reference Information</legend>
+    <div id="product-references">
+        <div class="product-reference-pair">
+            <div class="form-field">
+                <label for="{{ form.product_link.id }}">Product Reference</label>
+                {{ form.product_link(cols=40) }}
+            </div>
+    
+            <div class="form-field">
+                <label for="{{ form.link_description.id }}">Reference Description</label>
+                {{ form.link_description(cols=40) }}
+            </div>
+    
+            <div class="form-field buttons-row">
+                <button type="button" class="add-reference">Add more</button>
+                <button type="button" class="remove-reference" style="display: none;">Delete</button>
+            </div>
+        </div>
+    </div>
+    </fieldset>
+
+    <br><br>
+
+    <div class="form-group">
+        <div class="checkbox-field">
+            {{ form.deprecated() }}
+            <label for="{{ form.deprecated.id }}">{{ form.deprecated.label }}</label>
+        </div>
+    
+        <div class="checkbox-field">
+            {{ form.upcoming_change() }}
+            <label for="{{ form.upcoming_change.id }}">{{ form.upcoming_change.label }}</label>
+        </div>
+    </div>
+
+    <br><br>
+
+    <div class="form-group">
+        <div class="form-field">
+            <label for="{{ form.product_status.id }}">Status</label>
+            {{ form.product_status(id="status-dropdown", class="status-dropdown") }}
+        </div>
+
+        <div class="form-field">
+            <label for="{{ form.product_status_detail.id }}">Status details</label>
+            {{ form.product_status_detail(id="status-details-dropdown", class="status-details-dropdown") }}
+        </div>
+    </div>
+
+    <br><br>
+
+    <fieldset class="product-alias-group">
+        <legend>Product Alias Information</legend>
+        <label for="{{ form.alias_name.id }}">{{ form.alias_name.label }}</label>
+        {{ form.alias_name(cols=40) }}
         
-        # Add Product Notes
-        product_notes_data = form.product_notes.data
-        if product_notes_data:
-            product_notes = ProductNotes(product_id=new_product.product_id, product_note=product_notes_data)
-            db.session.add(product_notes)
+        <br><br>
+        
+        <label for="{{ form.alias_type.id }}">{{ form.alias_type.label }}</label>
+        {{ form.alias_type(id="alias-type-dropdown", class="alias-type-dropdown") }}
+        
+        <br><br>
 
-        # Add Product References
-        product_references_data = []
-        for i in range(len(request.form.getlist('product_link'))):
-            product_link = request.form.getlist('product_link')[i]
-            link_description = request.form.getlist('link_description')[i]
+        <div class="form-group">
+            <div class="checkbox-field">
+                {{ form.alias_approved() }}
+                <label for="{{ form.alias_approved.id }}" data-toggle="tooltip"
+                    title="Leaving it blank for unapproved aliases.">{{ form.alias_approved.label }}</label>
+            </div>
+        
+            <div class="checkbox-field">
+                {{ form.previous_name() }}
+                <label for="{{ form.previous_name.id }}">{{ form.previous_name.label }}</label>
+            </div>
+        
+            <div class="checkbox-field">
+                {{ form.tech_docs() }}
+                <label for="{{ form.tech_docs.id }}">{{ form.tech_docs.label }}</label>
+            </div>
+        
+            <div class="checkbox-field">
+                {{ form.tech_docs_cli() }}
+                <label for="{{ form.tech_docs_cli.id }}">{{ form.tech_docs_cli.label }}</label>
+            </div>
+        </div>
+        
+        <br><br>
 
-            if product_link and link_description:
-                product_references_data.append({
-                    'product_link': product_link,
-                    'link_description': link_description
-                })
+        <label for="{{ form.alias_notes.id }}">{{ form.alias_notes.label }}</label>
+        {{ form.alias_notes(cols=40) }}
 
-        for reference_data in product_references_data:
-            product_references = ProductReferences(
-                product_id=new_product.product_id,
-                product_link=reference_data['product_link'],
-                link_description=reference_data['link_description']
-            )
-            db.session.add(product_references)
+        <br>
+        <button type="button" class="add-alias-group" style="margin-top: 10px;">Add more aliases</button>
+        <button type="button" class="remove-alias-group" style="display: none;">Delete</button>
+    </fieldset>
 
-        # Print or log form data for debugging
-        print("Alias names:", request.form.getlist('alias_name'))
-        print("Alias types:", request.form.getlist('alias_type'))
+    <br><br>
 
-        product_alias_data = []
+    <fieldset class="product-release-info">
+        <legend>Product Release Information</legend>
+    
+        <label for="{{ form.product_release.id }}">{{ form.product_release.label }}</label>
+        {{ form.product_release() }}
+    
+        <br><br>
+    
+        <label for="{{ form.product_release_detail.id }}">{{ form.product_release_detail.label }}</label>
+        {{ form.product_release_detail(cols=40) }}
+    
+        <br><br>
+    
+        <label for="{{ form.product_release_link.id }}">{{ form.product_release_link.label }}</label>
+        {{ form.product_release_link(cols=40) }}
+    </fieldset>
+    
+    <fieldset class="product-eol-info">
+        <legend>Product End of Life Information</legend>
+    
+        <label for="{{ form.product_eol.id }}">{{ form.product_eol.label }}</label>
+        {{ form.product_eol() }}
+    
+        <br><br>
+    
+        <label for="{{ form.product_eol_detail.id }}">{{ form.product_eol_detail.label }}</label>
+        {{ form.product_eol_detail(cols=40) }}
+    
+        <br><br>
+    
+        <label for="{{ form.product_eol_link.id }}">{{ form.product_eol_link.label }}</label>
+        {{ form.product_eol_link(cols=40) }}
+    </fieldset>
 
-        alias_names = request.form.getlist('alias_name')
-        alias_types = request.form.getlist('alias_type')
-        alias_approved_list = request.form.getlist('alias_approved')
-        previous_name_list = request.form.getlist('previous_name')
-        tech_docs_list = request.form.getlist('tech_docs')
-        tech_docs_cli_list = request.form.getlist('tech_docs_cli')
-        alias_notes_list = request.form.getlist('alias_notes')
+    <br><br>
+  
+    {{ form.submit(style="background-color: #1a73e8; color: #ffffff;") }}
+</form>
 
-        alias_name = ""
-        alias_type = ""
-        alias_approved = False
-        previous_name = False
-        tech_docs = False
-        tech_docs_cli = False
-        alias_notes = ""
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="{{ url_for('static', filename='scripts/status.js') }}"></script>
+<script src="{{ url_for('static', filename='scripts/preferences.js') }}"></script>
+<script src="{{ url_for('static', filename='scripts/alias.js') }}"></script>
 
-        for i in range(len(alias_names)):
-            alias_name = alias_names[i]
-            alias_type = alias_types[i]
-            alias_approved = alias_approved_list[i].lower() == 'y' if i < len(alias_approved_list) else False
-            previous_name = previous_name_list[i].lower() == 'y' if i < len(previous_name_list) else False
-            tech_docs = tech_docs_list[i].lower() == 'y' if i < len(tech_docs_list) else False
-            tech_docs_cli = tech_docs_cli_list[i].lower() == 'y' if i < len(tech_docs_cli_list) else False
-            alias_notes = alias_notes_list[i] if i < len(alias_notes_list) else ''
+{% endif %}
 
-            if alias_type:
-                product_alias_data.append({
-                    'alias_name': alias_name,
-                    'alias_type': alias_type,
-                    'alias_approved': alias_approved,
-                    'previous_name': previous_name,
-                    'tech_docs': tech_docs,
-                    'tech_docs_cli': tech_docs_cli,
-                    'alias_notes': alias_notes
-                })
-
-        # Loop to save aliases
-        for alias_data in product_alias_data:
-                # Directly use boolean values, no need for lower()
-                alias_approved = alias_data['alias_approved']
-                previous_name = alias_data['previous_name']
-                tech_docs = alias_data['tech_docs']
-                tech_docs_cli = alias_data['tech_docs_cli']
-
-                # Create ProductAlias object and add to the session
-                product_alias = ProductAlias(
-                    product_id=new_product.product_id,
-                    alias_name=alias_data['alias_name'],
-                    alias_type=alias_data['alias_type'],
-                    alias_approved=alias_approved,
-                    previous_name=previous_name,
-                    tech_docs=tech_docs,
-                    tech_docs_cli=tech_docs_cli,
-                    alias_notes=alias_data['alias_notes']
-                )
-                db.session.add(product_alias)
-
-            # Commit changes outside the loop
-                db.session.commit()
-                
-        db.session.commit()
-
-        # Format dates for display
-        formatted_created_date = created_date.strftime('%Y-%m-%d')
-        formatted_last_updated_date = created_date.strftime('%Y-%m-%d')
-
-        # Set success message and hide the form
-        success_message = f'Successfully added the product: {form.product_name.data}'
-        show_form = False
-
-    return render_template('opl/add.html', form=form, success_message=success_message,
-                           show_form=show_form, formatted_created_date=formatted_created_date,
-                           formatted_last_updated_date=formatted_last_updated_date)
-
-# Define the route to edit a product
-@app.route('/opl/edit-product', methods=['GET', 'POST'])
-def edit_product():
-    form = MyForm()
-
-    if form.validate_on_submit():
-        # Logic for editing the product
-        return render_template('opl/edit.html', form=form, success_message=f'Successfully edited: {form.product_id.data}, {form.product_name.data}')
-
-    return render_template('opl/edit.html', form=form)
-
-# Run the application if executed directly
-if __name__ == '__main__':
-    with app.app_context():
-        # Create all database tables
-        db.create_all()
-    app.run(debug=True)
+{% if success_message %}
+<p class="success">{{ success_message }}</p>
+<br><br>
+<a href="{{ url_for('add_product') }}" class="button-link">Add more products</a>
+{% endif %}
+{% endblock %}
