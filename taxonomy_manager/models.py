d9f67b143ae711cf11962af2bdfb66759ca4f09f
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-01-05 18:54:52 +0530

    Update models.py

--- taxonomy_manager/models.py
+++ taxonomy_manager/models.py
@@ -1,11 +1,1 @@
-        # Add Product Mkt Life
-        product_mkt_life = ProductMktLife(
-            product_id=new_product.product_id,
-            product_release=form.product_release.data,
-            product_release_detail=form.product_release_detail.data,
-            product_release_link=form.product_release_link.data,
-            product_eol=form.product_eol.data,
-            product_eol_detail=form.product_eol_detail.data,
-            product_eol_link=form.product_eol_link.data
-        )
-        db.session.add(product_mkt_life)
+{{ form.submit(style="background-color: #0a63ca; color: #ffffff; font-size: 25px;") }}
