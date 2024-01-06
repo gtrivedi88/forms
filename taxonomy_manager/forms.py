74c3e47809240323aa98f14e85c1b87b4a88c6ad
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-01-06 15:18:39 +0530

    Update forms.py

--- taxonomy_manager/forms.py
+++ taxonomy_manager/forms.py
@@ -21,17 +21,22 @@ function updateStatusDetailsChoices() {
                 // For other statuses or non-'Deprecated' status, show all choices
                 statusDetailsDropdown.append($('<option>', { value: value, text: label }));
             }
-        });
-    }
-
-    if (selectedStatus === 'Deprecated') {
+        if (selectedStatus === 'Deprecated') {
             // If Deprecated is selected, show only 'NULL' in Status Details
             statusDetailsDropdown.append($('<option>', { value: 'Null', text: 'NULL' }));
         } else {
             // If any other status is selected, show all choices
             $.each(initialStatusDetailsChoices, function (value, label) {
                 statusDetailsDropdown.append($('<option>', { value: value, text: label }));
-            });
-
-         }
+        });
     }
+}
+
+    // Initial setup
+    updateStatusDetailsChoices();
+
+    // Attach event handler to Product Status dropdown change event
+    $('#status-dropdown').change(function () {
+        updateStatusDetailsChoices();
+    });
+});
