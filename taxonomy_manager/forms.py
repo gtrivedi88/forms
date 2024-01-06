e0d1c7f91c9039da0efd71b6be385a9e71093025
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-01-06 20:09:05 +0530

    Update forms.py

--- taxonomy_manager/forms.py
+++ taxonomy_manager/forms.py
@@ -1,37 +1,17 @@
-    # Add a new field for selecting a component
-    component = SelectField('Parent', choices=[('', 'Select')], validators=[Optional()])
-    component_type = SelectField('Component Type', choices=[('', 'Select'), ('component', 'Component'), ('operator', 'Operator'), ('variant', 'Variant')], validators=[Optional()])
+$(document).ready(function () {
+    // Add more Product Component Information groups
+    $(document).on("click", ".add-component-group", function () {
+        var newComponentGroup = $(".product-component-group:first").clone();
+        newComponentGroup.find('select').val('');
+        newComponentGroup.find('.remove-component-group').show();
+        newComponentGroup.find('.add-component-group').remove(); // Remove the Add button in the cloned group
+        $(".product-component-group:last").after(newComponentGroup);
+    });
 
-
-
-
-        # Add Product Components
-        product_components_data = []
-        component_ids = request.form.getlist('component')
-        component_types = request.form.getlist('component_type')
-
-        for i in range(len(component_ids)):
-            component_id = component_ids[i]
-            component_type = component_types[i]
-
-            # Check if component_id is not 'Select'
-            if component_id != 'Select':
-                product_components_data.append({
-                    'component_id': component_id,
-                    'component_type': component_type
-                })
-
-        # Loop to save components
-        for component_data in product_components_data:
-            component_id = component_data['component_id']
-            component_type = component_data['component_type']
-
-            # Check if component_id is not 'Select' before saving to the database
-            if component_id:
-                # Create ProductComponent object and add to the session
-                product_component = ProductComponents(
-                    product_id=new_product.product_id,
-                    component_id=component_id,
-                    component_type=component_type
-                )
-                db.session.add(product_component)
+    // Remove the current Product Component Information group
+    $(document).on("click", ".remove-component-group", function () {
+        if ($(".product-component-group").length > 1) {
+            $(this).closest(".product-component-group").remove();
+        }
+    });
+});
