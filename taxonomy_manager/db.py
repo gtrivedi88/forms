d9185972af99e31ea4c498b94ae1d143115faaf7
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-01-09 16:12:05 +0530

    Update db.py

--- taxonomy_manager/db.py
+++ taxonomy_manager/db.py
@@ -1,25 +1,35 @@
-    # Get product mkt life details
-    product_mkt_life = ProductMktLife.query.filter_by(product_id=product_id).first()
+lass ProductMktLife(db.Model):
+    """
+    Represents marketing life information for a product.
+
+    Attributes:
+    - product_id: Foreign key to Product.
+    - product_release: Release date of the product.
+    - product_release_detail: Details about the product release.
+    - product_release_link: Reference link for the product release.
+    - product_eol: End of Life date of the product.
+    - product_eol_detail: Details about the End of Life of the product.
+    - product_eol_link: Reference link for the End of Life of the product.
+
+    Relationships:
+    - product: Relationship to Product for easy access to the associated product.
+    """
+
+    __tablename__ = 'product_mkt_life'
+    __table_args__ = {'schema': 'brand_opl'}
+
+    product_id = db.Column(db.String, db.ForeignKey('brand_opl.product.product_id'), primary_key=True)
+    product_release = db.Column(db.Date, nullable=True)
+    product_release_detail = db.Column(db.String(255))
+    product_release_link = db.Column(db.String(255))
+    product_eol = db.Column(db.Date, nullable=True)
+    product_eol_detail = db.Column(db.String(255))
+    product_eol_link = db.Column(db.String(255))
 
 
-<fieldset class="product-release-info">
-        <legend>Product Release Information</legend>
-    
-        <label for="{{ form.product_release.id }}">{{ form.product_release.label }}</label>
-        {{ form.product_release() }}
-    
-        <br>
-    
-        <label for="{{ form.product_release_detail.id }}">{{ form.product_release_detail.label }}</label>
-        {{ form.product_release_detail(cols=40) }}
-    
-        <br>
-    
-        <label for="{{ form.product_release_link.id }}">{{ form.product_release_link.label }}</label>
-        {{ form.product_release_link(cols=40) }}
-    </fieldset>
-    
-    <fieldset class="product-eol-info">
+
+
+<fieldset class="product-eol-info">
         <legend>Product End of Life Information</legend>
     
         <label for="{{ form.product_eol.id }}">{{ form.product_eol.label }}</label>
@@ -35,5 +45,3 @@
         <label for="{{ form.product_eol_link.id }}">{{ form.product_eol_link.label }}</label>
         {{ form.product_eol_link(cols=40) }}
     </fieldset>
-
-    <br><br>
