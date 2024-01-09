7a51dce72d6942767403bb013dcea5c48b68c49b
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-01-09 16:01:05 +0530

    Update db.py

--- taxonomy_manager/db.py
+++ taxonomy_manager/db.py
@@ -1,48 +1,39 @@
-        # Add Product References
-        product_references_data = []
-        for i in range(len(request.form.getlist('product_link'))):
-            product_link = request.form.getlist('product_link')[i]
-            link_description = request.form.getlist('link_description')[i]
-
-            if product_link and link_description:
-                product_references_data.append({
-                    'product_link': product_link,
-                    'link_description': link_description
-                })
-
-        for reference_data in product_references_data:
-            product_references = ProductReferences(
-                product_id=new_product.product_id,
-                product_link=reference_data['product_link'],
-                link_description=reference_data['link_description']
-            )
-            db.session.add(product_references)
+    # Get product mkt life details
+    product_mkt_life = ProductMktLife.query.filter_by(product_id=product_id).first()
 
 
+<fieldset class="product-release-info">
+        <legend>Product Release Information</legend>
+    
+        <label for="{{ form.product_release.id }}">{{ form.product_release.label }}</label>
+        {{ form.product_release() }}
+    
+        <br>
+    
+        <label for="{{ form.product_release_detail.id }}">{{ form.product_release_detail.label }}</label>
+        {{ form.product_release_detail(cols=40) }}
+    
+        <br>
+    
+        <label for="{{ form.product_release_link.id }}">{{ form.product_release_link.label }}</label>
+        {{ form.product_release_link(cols=40) }}
+    </fieldset>
+    
+    <fieldset class="product-eol-info">
+        <legend>Product End of Life Information</legend>
+    
+        <label for="{{ form.product_eol.id }}">{{ form.product_eol.label }}</label>
+        {{ form.product_eol() }}
+    
+        <br>
+    
+        <label for="{{ form.product_eol_detail.id }}">{{ form.product_eol_detail.label }}</label>
+        {{ form.product_eol_detail(cols=40) }}
+    
+        <br>
+    
+        <label for="{{ form.product_eol_link.id }}">{{ form.product_eol_link.label }}</label>
+        {{ form.product_eol_link(cols=40) }}
+    </fieldset>
 
-
-
-class ProductReferences(db.Model):
-    """
-    Represents references associated with a product.
-
-    Attributes:
-    - product_id: Foreign key to Product.
-    - product_link: Link associated with the product reference.
-    - link_description: Description associated with the product reference.
-
-    Relationships:
-    - product: Relationship to Product for easy access to the associated product references.
-    """
-
-    __tablename__ = 'product_references'
-    __table_args__ = {'schema': 'brand_opl'}
-    __uuid__ = "product_id"
-    __term__ = "product_link"
-
-    product_id = db.Column(db.String, db.ForeignKey('brand_opl.product.product_id'), primary_key=True)
-    product_link = db.Column(db.String(65535), nullable=False)
-    link_description = db.Column(db.String(65535), nullable=False)
-
-    # The relationship to the Product model
-    product = db.relationship('Product', backref=db.backref('product_references', lazy='dynamic'))
+    <br><br>
