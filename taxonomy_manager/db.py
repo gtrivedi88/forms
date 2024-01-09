7e0b6d0bcaf8ea93e8db5cf1ffb4d94f0618b782
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-01-09 16:17:08 +0530

    Update db.py

--- taxonomy_manager/db.py
+++ taxonomy_manager/db.py
@@ -1,47 +1,33 @@
-lass ProductMktLife(db.Model):
+<fieldset class="product-partner-group">
+    <legend>Product Partners Information</legend>
+        <div class="form-field">
+            <label for="{{ form.partner.id }}">{{ form.partner.label }}</label>
+            <select id="{{ form.partner.id }}" name="{{ form.partner.name }}" multiple>
+                {% for value, label in form.partner.choices %}
+                <option value="{{ value }}">{{ label }}</option>
+                {% endfor %}
+            </select>
+        </div>
+    </fieldset>
+
+
+    class Partner(db.Model):
     """
-    Represents marketing life information for a product.
+    Represents partners.
 
     Attributes:
-    - product_id: Foreign key to Product.
-    - product_release: Release date of the product.
-    - product_release_detail: Details about the product release.
-    - product_release_link: Reference link for the product release.
-    - product_eol: End of Life date of the product.
-    - product_eol_detail: Details about the End of Life of the product.
-    - product_eol_link: Reference link for the End of Life of the product.
-
-    Relationships:
-    - product: Relationship to Product for easy access to the associated product.
+    - partner_id: Unique identifier for the partner.
+    - partner_name: Name of the partner.
+    - persona_id: Persona ID for the partner.
     """
 
-    __tablename__ = 'product_mkt_life'
+    __tablename__ = 'partners'
     __table_args__ = {'schema': 'brand_opl'}
+    __uuid__ = "partner_id"
+    __term__ = "partner_name"
 
-    product_id = db.Column(db.String, db.ForeignKey('brand_opl.product.product_id'), primary_key=True)
-    product_release = db.Column(db.Date, nullable=True)
-    product_release_detail = db.Column(db.String(255))
-    product_release_link = db.Column(db.String(255))
-    product_eol = db.Column(db.Date, nullable=True)
-    product_eol_detail = db.Column(db.String(255))
-    product_eol_link = db.Column(db.String(255))
+    partner_id = db.Column(db.String, primary_key=True)
+    partner_name = db.Column(db.String(255), nullable=False)
 
-
-
-
-<fieldset class="product-eol-info">
-        <legend>Product End of Life Information</legend>
-    
-        <label for="{{ form.product_eol.id }}">{{ form.product_eol.label }}</label>
-        {{ form.product_eol() }}
-    
-        <br>
-    
-        <label for="{{ form.product_eol_detail.id }}">{{ form.product_eol_detail.label }}</label>
-        {{ form.product_eol_detail(cols=40) }}
-    
-        <br>
-    
-        <label for="{{ form.product_eol_link.id }}">{{ form.product_eol_link.label }}</label>
-        {{ form.product_eol_link(cols=40) }}
-    </fieldset>
+    # Relationships
+    product_partners = db.relationship('ProductPartners', backref='partner', lazy='dynamic')
